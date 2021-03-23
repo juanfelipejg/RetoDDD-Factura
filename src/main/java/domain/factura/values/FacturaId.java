1eff4349800f23e5a7e7d40d65823a7c7eb44947
jjaramga <juanfelipejg@gmail.com>
2021-03-22 22:02:43 -0500

    Create value object FacturaId

---
+++ src/main/java/domain/factura/values/FacturaId.java
@@ -0,0 +1,17 @@
+package domain.factura.values;
+
+import co.com.sofka.domain.generic.Identity;
+
+public class FacturaId extends Identity {
+
+    private FacturaId(String uid){
+        super(uid);
+    }
+
+    public FacturaId(){
+    }
+
+    public static FacturaId of(String uid){
+        return new FacturaId(uid);
+    }
+}
