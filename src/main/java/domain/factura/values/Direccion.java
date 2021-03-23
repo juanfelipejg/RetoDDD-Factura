874b39767e06f563a782ff29e555ce07d42b4746
jjaramga <juanfelipejg@gmail.com>
2021-03-22 21:45:32 -0500

    Create value object Direccion

---
+++ src/main/java/domain/factura/values/Direccion.java
@@ -0,0 +1,17 @@
+package domain.factura.values;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+public class Direccion implements ValueObject<String> {
+
+    private final String value;
+
+    public Direccion(String direccion) {
+        this.value = direccion;
+    }
+
+    @Override
+    public String value() {
+        return value;
+    }
+}
