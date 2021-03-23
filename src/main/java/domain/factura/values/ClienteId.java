5c59c46687dab37d798e5fb217d18fc6899ec836
jjaramga <juanfelipejg@gmail.com>
2021-03-23 11:21:35 -0500

    Create value object ClienteId

---
+++ src/main/java/domain/factura/values/ClienteId.java
@@ -0,0 +1,19 @@
+package domain.factura.values;
+
+import co.com.sofka.domain.generic.Identity;
+
+public class ClienteId extends Identity {
+
+    private ClienteId(String uid){
+        super(uid);
+    }
+
+    public ClienteId(){
+
+    }
+
+    public static ClienteId of(String uid){
+        return new ClienteId(uid);
+    }
+
+}
