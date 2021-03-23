ab906a7abfe0bc865896954370ddc968ff496364
jjaramga <juanfelipejg@gmail.com>
2021-03-22 19:28:55 -0500

    Create value object Nombre

---
+++ src/main/java/domain/factura/values/Nombre.java
@@ -0,0 +1,22 @@
+package domain.factura.values;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Nombre implements ValueObject<String> {
+
+    private final String value;
+
+    public Nombre(String nombre) {
+        this.value = Objects.requireNonNull(nombre);
+        if (nombre.isBlank()) {
+            throw new IllegalArgumentException("El nombre no puede esta vacio");
+        }
+    }
+
+    @Override
+    public String value() {
+        return value;
+    }
+}
