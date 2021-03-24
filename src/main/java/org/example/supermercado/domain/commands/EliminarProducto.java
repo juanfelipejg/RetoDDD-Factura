f49121d7a6083c56d1cd01399e91ef2d64e30f40
jjaramga <juanfelipejg@gmail.com>
2021-03-24 16:36:10 -0500

    Create command Eliminar producto

---
+++ src/main/java/org/example/supermercado/domain/commands/EliminarProducto.java
@@ -0,0 +1,41 @@
+package org.example.supermercado.domain.commands;
+
+import co.com.sofka.domain.generic.Command;
+import org.example.supermercado.domain.values.*;
+
+public class EliminarProducto implements Command {
+
+    private final FacturaId facturaId;
+    private final ProductoId productoId;
+    private final Nombre nombre;
+    private final Valor precio;
+    private final Descripcion descripcion;
+
+    public EliminarProducto(FacturaId facturaId, ProductoId productoId, Nombre nombre, Valor precio, Descripcion descripcion) {
+        this.productoId = productoId;
+        this.facturaId = facturaId;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+    }
+
+    public ProductoId getProductoId() {
+        return productoId;
+    }
+
+    public FacturaId getFacturaId() {
+        return facturaId;
+    }
+
+    public Nombre getNombre() {
+        return nombre;
+    }
+
+    public Valor getPrecio() {
+        return precio;
+    }
+
+    public Descripcion getDescripcion() {
+        return descripcion;
+    }
+}
---
+++ src/main/java/org/example/supermercado/usecases/EliminarProductoUseCase.java
@@ -0,0 +1,8 @@
+package org.example.supermercado.usecases;
+
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import org.example.supermercado.domain.commands.EliminarProducto;
+
+public class EliminarProductoUseCase extends UseCase<RequestCommand<EliminarProducto>, ResponseEvents>{
+}
